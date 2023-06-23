15cb6d292f290736966fb64a387e956070856f27
MyoNyiNyiNaing <mgmyonyinyi308@gmail.com>
2023-06-23 22:24:30 +0630

    add saleChart and donut chart

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,35 +2,37 @@ import React from "react";
 import { BsArrowDown, BsQuestionCircleFill } from "react-icons/bs";
 import BarChart from "./Chart/BarChart";
 
-const Card = () => {
+const Card = ({data, title, rate, month, monthCount, week, weekCount, decRate}) => {
   return (
-    <div className="col-span-4 bg-white border border-[#dbdfea] rounded p-5 text-[#364a63]">
+    <div className=" bg-white border border-[#dbdfea] rounded p-5 text-[#364a63]">
       <div className=" flex flex-col gap-1">
         <div className="flex justify-between items-center ">
-          <h1 className="font-nunito text-[17px] font-bold ">Total Booking</h1>
+          <h1 className="font-nunito text-[17px] font-bold ">{title}</h1>
           <BsQuestionCircleFill />
         </div>
         <div className="flex items-center gap-2">
-          <p className="font-roboto text-2xl leading-10">11,230</p>
+          <p className="font-roboto text-2xl leading-10">{rate}</p>
           <span className="flex items-center gap-1 text-[13px] text-[#e85347] leading-[14px]">
-            <BsArrowDown /> 1.93%
+            {decRate && <BsArrowDown />} {decRate}
           </span>
         </div>
-        <div className="flex justify-between items-end">
-          <div>
-            <h1 className="text-[11px] text-[#8094ae] tracking-widest">
-              THIS MONTH
-            </h1>
-            <p className=" text-[15px] text-[#364a63]">1913</p>
+        <div className=" flex justify-between mt-4">
+          <div className=" space-y-3">
+            <div>
+              <h1 className="text-[11px] text-[#8094ae] tracking-widest">
+                {month}
+              </h1>
+              <p className=" text-[15px] text-[#364a63]">{monthCount}</p>
+            </div>
+            <div>
+              <h1 className="text-[11px] text-[#8094ae] tracking-widest">
+                {week}
+              </h1>
+              <p className=" text-[15px] text-[#364a63]">{weekCount}</p>
+            </div>
           </div>
-          <div>
-            <h1 className="text-[11px] text-[#8094ae] tracking-widest">
-              THIS WEEK
-            </h1>
-            <p className=" text-[15px] text-[#364a63]">1125</p>
-          </div>
-          <div className="w-[100px] h-[50px] overflow-hidden">
-            <BarChart />
+          <div className=" w-[100px] h-[75px] overflow-hidden mt-2">
+            <BarChart data={data}/>
           </div>
         </div>
       </div>
---
+++ src/components/Chart/SaleRevenueChart.jsx
@@ -0,0 +1,105 @@
+import React from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+  maintainAspectRatio: false,
+  plugins: {
+    legend: false,
+    title: {
+      display: false,
+      
+    },
+    tooltip: {
+      callbacks: {
+         title : () => null // or function () { return null; }
+      }
+   }
+   
+  },
+
+  scales: {
+    x: {
+      display: false,
+      grid: {
+        display: false,
+      },
+    },
+    y: {
+      display: false,
+      grid: {
+        display: false,
+      },
+    },
+  },
+};
+const labels = [
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "11",
+  "12",
+  "13",
+  "14",
+  "15",
+  "16",
+  "17",
+  "18",
+  "19",
+  "20",
+  "21",
+  "22",
+  "23",
+  "24",
+  "25",
+  "26",
+  "27",
+  "28",
+  "29",
+  "30",
+];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      data: [
+        1110, 1220, 1320, 980, 900, 720, 1060, 830, 890, 555, 789, 1190, 800,
+        1250, 1130, 850, 750, 560, 810, 760, 1250, 1310, 1110, 980, 960, 720,
+        980, 800, 650, 860,
+      ],
+      backgroundColor: "#9CABFF",
+      barThickness: 6,
+    },
+  ],
+};
+
+const SaleRevenueChart = () => {
+  return <Bar options={options} data={data}/>;
+};
+export default SaleRevenueChart;
